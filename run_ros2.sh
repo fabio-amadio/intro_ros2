b1e6a82f9ea0a71a6809ec455af476667a981d87
Fabio Amadio <fabioamadio93@gmail.com>
2025-12-23 12:55:16 +0100

    Init

---
+++ run_ros2.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME=intro-ros2
+IMAGE_NAME=intro-ros2:humble
+COLCON_WS=/root/ros2_ws
+
+isRunning=$(docker ps -f name=${CONTAINER_NAME} | grep -c ${CONTAINER_NAME})
+
+if [ "$isRunning" -eq 0 ]; then
+    echo "Starting ROS 2 Humble container..."
+
+    # Allow Docker to access X server (optional, for RViz / GUI tools)
+    xhost +local:docker >/dev/null
+
+    # Remove stopped container if it exists
+    docker rm ${CONTAINER_NAME} >/dev/null 2>&1
+
+    docker run \
+        -it \
+        --name ${CONTAINER_NAME} \
+        --network host \
+        --ipc host \
+        -e DISPLAY=${DISPLAY} \
+        -e QT_X11_NO_MITSHM=1 \
+        -e ROS_DOMAIN_ID=30 \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        -v ./exercise_pub_sub:${COLCON_WS}/src/exercise_pub_sub \
+        --entrypoint /bin/bash \
+        ${IMAGE_NAME}
+
+else
+    echo "ROS 2 container already running."
+    docker exec -it ${CONTAINER_NAME} /bin/bash
+fi
+
